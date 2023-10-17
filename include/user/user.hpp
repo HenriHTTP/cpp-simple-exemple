64b7acb1ee69d774a9955e748f2cbba32d4b5f49
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-16 22:05:22 -0300

    refactor:  add spaces

--- include/user/user.hpp
+++ include/user/user.hpp
@@ -12,6 +12,7 @@ class userInterfaces {
   virtual void setName(const std::string &name) = 0;
   virtual void setType(const std::string &type) = 0;
   virtual void setState(const bool &state) = 0;
+
   virtual std::string getName() = 0;
   virtual std::string getType() = 0;
   virtual bool getState() = 0;
@@ -25,6 +26,7 @@ class userClass : public userInterfaces {
   void setName(const std::string &name) override;
   void setType(const std::string &type) override;
   void setState(const bool &tyoe) override;
+
   std::string getName() override;
   std::string getType() override;
   bool getState() override;
@@ -34,6 +36,7 @@ class userClass : public userInterfaces {
   std::string userType{};
   bool userState{};
 };
+
 }  // namespace user
 
 #endif  // _USER_H
