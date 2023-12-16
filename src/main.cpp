5d974b16bc66fce331c146acbcb611466434a348
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-12-16 17:49:15 -0300

    feat: remove comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,27 @@
 #include "../include/user/user.hpp"
 
 int main() {
-  std::cout << hello::hello_world() << std::endl;
-  std::cout << "my rina not like me too , i wanna cry" << std::endl;
+  // std::cout << hello::hello_world() << std::endl;
+  // std::cout << "my rina not like me too , i wanna cry" << std::endl;
 
-  user::userClass user{};
-  user.setName("rina_wilk");
+  // user::userClass user{};
+  // user.setName("rina_wilk");
 
-  std::cout << user.getName() << std::endl;
+  // std::cout << user.getName() << std::endl;
 
-  player::playerClass player{};
-  std::cout << "position:" << player.getPositionPlayer() << std::endl;
-  player.moveLeft(10);
-  std::cout << "position:" << player.getPositionPlayer() << std::endl;
-  player.moveRight(20);
-  std::cout << "position:" << player.getPositionPlayer() << std::endl;
+  player::player *player = new player::player;
 
-  if(player.getPositionPlayer() > 0){ 
-    std::cout<< "position player is right"<<std::endl;
-  }else{ 
-    std::cout<< "position player is left"<<std::endl;
-  }
+  std::cout << "position:" << player->get_position_x() << std::endl;
 
+  player->move_left(20);
+  std::cout << "position:" << player->get_position_x() << std::endl;
+
+  player->move_right(20);
+  std::cout << "position:" << player->get_position_x() << std::endl;
+
+  player->set_speed(3.16f);
+  std::cout << "speed:" << player->get_speed() << std::endl;
+
+  delete player;
   return 0;
 }
