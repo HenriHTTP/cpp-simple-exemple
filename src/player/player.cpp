62fd1004527a789e9c15a529adc32494f5aeb726
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-12-16 17:47:58 -0300

    refactor:  validation position method set

--- src/player/player.cpp
+++ src/player/player.cpp
@@ -1,6 +1,31 @@
 #include "../../include/player/player.hpp"
 
-void player::playerClass::moveLeft(const int &position) { x -= position; }
-void player::playerClass::moveRight(const int &position) { x += position; }
+namespace player {
+void player::move_left(const int &position) {
+  if (this->x >= 0 && this->x != 0) {
+    this->x -= position;
+  }
+  this->x;
+}
 
-int player::playerClass::getPositionPlayer() { return x; }
+void player::move_right(const int &position) {
+  if (this->x >= 0) {
+    this->x += position;
+  }
+  this->x;
+}
+
+void player::move_jump(const int &jump_height) {
+  if (this->y == 0) {
+    this->y = jump_height;
+  }
+  this->x;
+}
+
+void player::set_speed(const float &speed) { this->speed = speed; }
+
+auto player::get_position_x() -> const int { return this->x; }
+auto player::get_positon_y() -> const int { return this->y; }
+auto player::get_speed() -> const float { return this->speed; }
+
+}  // namespace player
